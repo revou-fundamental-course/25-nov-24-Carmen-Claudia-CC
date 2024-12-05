d8c4105ad02583cb9ceab6f88912056baa8d8f82
Carmen-Claudia-CC <cccarmenclaudia@gmail.com>
2024-12-05 16:55:38 +0700

    Edit 2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function kalkulasiBMI() {
     var berat = document.getElementById("berat").value;
     var tinggi = document.getElementById("tinggi").value;
     var BMI = (parseInt(berat) / (parseInt(tinggi))**2) * 10000;
-    var resultDiv = document.getElementById("output"); <!-----wadah mau taru hasil disebut dulu>
-    resultDiv.innerText = BMI.toFixed(2);
+    // wadah buat diisi hasil disebut dulu
+    var resultDiv = document.getElementById("output"); 
+    resultDiv.innerText = BMI.toFixed(1);
     }
